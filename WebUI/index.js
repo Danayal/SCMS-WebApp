9c84284cc787dfc8aad9c9034237a3984502f562
danayal <danayalkh@gmail.com>
2020-03-10 20:42:13 +0400

    Update

--- WebUI/index.js
+++ WebUI/index.js
@@ -1,11 +1,28 @@
 $(document).ready(() => {
-$("#registerBtn").click(()=> {
-    $.post("/admin/signUp", {
-        admin_email: $("#inputEmail").val(),
-        password: $("#inputPassword").val(),
-        verificationCode: $("#inputVerification").val()
-    }, (data, status) => {
-        alert(JSON.stringify(data));
+    console.log("I am there");
+    $("#registerBtn").click((e) => {
+        $.post("/admin/signUp", {
+            admin_email: $("#inputEmail").val(),
+            password: $("#inputPassword").val(),
+            verificationCode: $("#inputVerification").val()
+        }, (data, status) => {
+            alert(JSON.stringify(data));
+        })
+    });
+
+    $("#loginBtn").click(() => {
+        console.log("login clicked");
+        $.post("/admin/login", {
+            admin_email: $("#inputEmailSignIn").val(),
+            password: $("#inputPasswordSignIn").val()
+        }, (data, status) => {
+            if(data == "Login successful"){
+                window.location.href = '/index.html'
+            }else{
+                alert(data);
+            }
+            
+        })
     })
-}
+
 })
